03d6f74292c450d5ed276d6b150327db4409246e
kyle olson <olso4051@umn.edu>
2012-12-10 00:12:12 -0600

    random math question onResume, and checks correctness of answer

--- src/com/olyware/mathlock/MainActivity.java
+++ src/com/olyware/mathlock/MainActivity.java
@@ -23,7 +23,6 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private TextView clock;
@@ -34,15 +33,13 @@ public class MainActivity extends Activity {
 	private TextView problem;
 	private RadioGroup radioGroup2;
 	private RadioButton radioAnswer[] = new RadioButton[4];
-	// private RadioButton radioAnswer2;
-	// private RadioButton radioAnswer3;
-	// private RadioButton radioAnswer4;
 	private Button buttonUnlock;
 	private Button buttonSure;
+
 	private String PackageKeys[] = { "enable_math", "enable_vocab", "enable_translate" };
 	private String DifficultyKeys[] = { "difficulty_math", "difficulty_vocab", "difficulty_translate" };
 	private AudioManager am;
-	private Random rng = new Random(); // Ideally just create one instance globally
+	private Random rand = new Random(); // Ideally just create one instance globally
 
 	private int answerLoc = 1;		// {correct radiobutton location}
 	private String answers[] = { "3", "1", "2", "4" };	// { answers}
@@ -123,15 +120,15 @@ public class MainActivity extends Activity {
 		registerReceiver(m_timeChangedReceiver, s_intentFilter);
 
 		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-		// TODO set problem, answer, and wrong answers
-		setProblemAndAnswer();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+		// TODO set problem, answer, and wrong answers
+		setProblemAndAnswer();
 	}
 
 	@Override
@@ -208,10 +205,17 @@ public class MainActivity extends Activity {
 			radioGroup1.clearCheck();
 		} else {
 			// TODO check if answer is correct
-			buttonUnlock.setEnabled(false);
-			buttonSure.setEnabled(false);
-			radioGroup2.clearCheck();
-			launchHomeScreen();
+			if (radioAnswer[answerLoc].isChecked()) {
+				buttonUnlock.setEnabled(false);
+				buttonSure.setEnabled(false);
+				radioGroup2.clearCheck();
+				launchHomeScreen();
+			} else {
+				buttonUnlock.setEnabled(false);
+				buttonSure.setEnabled(false);
+				radioGroup2.clearCheck();
+				setProblemAndAnswer();
+			}
 		}
 	}
 
@@ -238,7 +242,7 @@ public class MainActivity extends Activity {
 			count = 0;
 
 			for (int i = 0; i < PackageKeys.length; i++) {
-				if (sharedPrefs.getBoolean("enable_math", false)) {
+				if (sharedPrefs.getBoolean(PackageKeys[i], false)) {
 					EnabledPackageKeys[count] = PackageKeys[i];
 					location[count] = i;
 					count++;
@@ -246,7 +250,7 @@ public class MainActivity extends Activity {
 			}
 
 			// pick a random enabled package
-			int randPack = rng.nextInt(EnabledPackageKeys.length);
+			int randPack = rand.nextInt(EnabledPackageKeys.length);
 			switch (location[randPack]) {
 			case 0:			// math question
 				setMathProblem(Integer.parseInt(sharedPrefs.getString(DifficultyKeys[0], "1")));
@@ -261,7 +265,7 @@ public class MainActivity extends Activity {
 				break;
 			}
 
-			answerLoc = rng.nextInt(3);			// set a random location for the correct answer
+			answerLoc = rand.nextInt(3);			// set a random location for the correct answer
 			int offset = 1;
 			for (int i = 0; i < 4; i++) {
 				if (i == answerLoc) {
@@ -278,31 +282,30 @@ public class MainActivity extends Activity {
 		int operator = 0;
 		int first = 1;
 		int second = 1;
-		Toast.makeText(this, String.valueOf(diffNum), Toast.LENGTH_SHORT).show();
 
 		switch (diffNum) {
 		case 1:				// Easy question
 			// add and subtract options
-			operator = rng.nextInt(1);
-			first = rng.nextInt(10);					// 0 through 10
-			second = rng.nextInt(10);					// 0 through 10
+			operator = rand.nextInt(1);
+			first = rand.nextInt(10);					// 0 through 10
+			second = rand.nextInt(10);					// 0 through 10
 			break;
 		case 2:				// Medium question
 			// add, subtract, multiply options
-			operator = rng.nextInt(2);
-			first = rng.nextInt(40) - 20;				// -20 through 20
-			second = rng.nextInt(40) - 20;				// -20 through 20
+			operator = rand.nextInt(2);
+			first = rand.nextInt(40) - 20;				// -20 through 20
+			second = rand.nextInt(40) - 20;				// -20 through 20
 			break;
 		case 3:				// Hard question
 			// add, subtract, multiply, and divide options
-			operator = rng.nextInt(3);
-			first = rng.nextInt(200) - 100;				// -100 through 100
-			second = rng.nextInt(200) - 100;			// -100 through 100
+			operator = rand.nextInt(3);
+			first = rand.nextInt(200) - 100;			// -100 through 100
+			second = rand.nextInt(200) - 100;			// -100 through 100
 			if (operator == 3) {
 				// check that answer will be an integer
 				while (first % second != 0) {
-					first = rng.nextInt(200) - 100;		// new numbers
-					second = rng.nextInt(200) - 100;
+					first = rand.nextInt(200) - 100;	// new numbers
+					second = rand.nextInt(200) - 100;
 				}
 			}
 			break;
@@ -331,7 +334,7 @@ public class MainActivity extends Activity {
 		List<Integer> generated = new ArrayList<Integer>();
 		for (int i = 0; i < 3; i++) {
 			while (true) {
-				int offset = rng.nextInt(20) - 10;
+				int offset = rand.nextInt(20) - 10;
 				if (!generated.contains(offset) && offset != 0) {
 					// Done for this iteration
 					generated.add(offset);
